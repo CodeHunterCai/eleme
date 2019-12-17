7f889c6d54654b51f42b54918ed67a0761140010
cyp <1203095671@qq.com>
2019-12-17 16:47:45 +0800

    购物车弹窗功能

--- src/components/shop-cart/shop-cart.vue
+++ src/components/shop-cart/shop-cart.vue
@@ -14,7 +14,7 @@
           <div class="price" :class="{'highlight':totalPrice>0}">￥{{totalPrice}}</div>
           <div class="desc">另需配送费￥{{deliveryPrice}}元</div>
         </div>
-        <div class="content-right">
+        <div class="content-right" @click="pay">
           <div class="pay" :class="payClass">
             {{payDesc}}
           </div>
@@ -23,10 +23,9 @@
       <div class="ball-container">
         <div v-for="(ball,index) in balls" :key="index">
           <transition
-                  @before-enter="beforeDrop"
-                  @enter="dropping"
-                  @after-enter="afterDrop"
-          >
+            @before-enter="beforeDrop"
+            @enter="dropping"
+            @after-enter="afterDrop">
             <div class="ball" v-show="ball.show">
               <div class="inner inner-hook"></div>
             </div>
@@ -38,145 +37,199 @@
 </template>
 
 <script>
-import Bubble from 'components/bubble/bubble'
+  import Bubble from 'components/bubble/bubble'
 
-const BALL_LEN = 10
-const innerClsHook = 'inner-hook'
+  const BALL_LEN = 10
+  const innerClsHook = 'inner-hook'
 
-function createBalls () {
-  let ret = []
-  for (let i = 0; i < BALL_LEN; i++) {
-    ret.push({
-      show: false
-    })
+  function createBalls() {
+    let balls = []
+    for (let i = 0; i < BALL_LEN; i++) {
+      balls.push({show: false})
+    }
+    return balls
   }
-  return ret
-}
 
-export default {
-  name: 'shop-cart',
-  props: {
-    selectFoods: {
-      type: Array,
-      default () {
-        return []
+  export default {
+    name: 'shop-cart',
+    props: {
+      selectFoods: {
+        type: Array,
+        default() {
+          return []
+        }
+      },
+      deliveryPrice: {
+        type: Number,
+        default: 0
+      },
+      minPrice: {
+        type: Number,
+        default: 0
+      },
+      sticky: {
+        type: Boolean,
+        default: false
+      },
+      fold: {
+        type: Boolean,
+        default: true
       }
     },
-    deliveryPrice: {
-      type: Number,
-      default: 0
-    },
-    minPrice: {
-      type: Number,
-      default: 0
-    }
-  },
-  data () {
-    return {
-      balls: createBalls()
-    }
-  },
-  computed: {
-    totalPrice () {
-      let total = 0
-      this.selectFoods.forEach((food) => {
-        total += food.price * food.count
-      })
-      return total
-    },
-    totalCount () {
-      let count = 0
-      this.selectFoods.forEach((food) => {
-        count += food.count
-      })
-      return count
-    },
-    payDesc () {
-      if (this.totalPrice === 0) {
-        return `￥${this.minPrice}元起送`
-      } else if (this.totalPrice < this.minPrice) {
-        let diff = this.minPrice - this.totalPrice
-        return `还差￥${diff}元起送`
-      } else {
-        return '去结算'
+    data() {
+      return {
+        balls: createBalls(),
+        listFold: this.fold
       }
     },
-    payClass () {
-      if (!this.totalCount || this.totalPrice < this.minPrice) {
-        return 'not-enough'
-      } else {
-        return 'enough'
+    created() {
+      this.dropBalls = []
+    },
+    computed: {
+      totalPrice() {
+        let total = 0
+        this.selectFoods.forEach((food) => {
+          total += food.price * food.count
+        })
+        return total
+      },
+      totalCount() {
+        let count = 0
+        this.selectFoods.forEach((food) => {
+          count += food.count
+        })
+        return count
+      },
+      payDesc() {
+        if (this.totalPrice === 0) {
+          return `￥${this.minPrice}元起送`
+        } else if (this.totalPrice < this.minPrice) {
+          let diff = this.minPrice - this.totalPrice
+          return `还差￥${diff}元起送`
+        } else {
+          return '去结算'
+        }
+      },
+      payClass() {
+        if (!this.totalCount || this.totalPrice < this.minPrice) {
+          return 'not-enough'
+        } else {
+          return 'enough'
+        }
       }
-    }
-  },
-  created () {
-    this.dropBalls = []
-    this.listFold = true
-  },
-  methods: {
-    drop (el) {
-      for (let i = 0; i < this.balls.length; i++) {
-        const ball = this.balls[i]
-        if (!ball.show) {
-          ball.show = true
-          ball.el = el
-          this.dropBalls.push(ball)
+    },
+    methods: {
+      toggleList() {
+        if (this.listFold) {
+          if (!this.totalCount) {
+            return
+          }
+          this.listFold = false
+          this._showShopCartList()
+          this._showShopCartSticky()
+        } else {
+          this.listFold = true
+          this._hideShopCartList()
+        }
+      },
+      pay(e) {
+        if (this.totalPrice < this.minPrice) {
           return
         }
-      }
-    },
-    beforeDrop (el) {
-      const ball = this.dropBalls[this.dropBalls.length - 1]
-      const rect = ball.el.getBoundingClientRect()
-      const x = rect.left - 32
-      const y = -(window.innerHeight - rect.top - 22)
-      el.style.display = ''
-      el.style.transform = el.style.webkitTransform = `translate3d(0,${y}px,0)`
-      const inner = el.getElementsByClassName(innerClsHook)[0]
-      inner.style.transform = inner.style.webkitTransform = `translate3d(${x}px,0,0)`
-    },
-    dropping (el, done) {
-      this._reflow = document.body.offsetHeight
-      el.style.transform = el.style.webkitTransform = `translate3d(0,0,0)`
-      const inner = el.getElementsByClassName(innerClsHook)[0]
-      inner.style.transform = inner.style.webkitTransform = `translate3d(0,0,0)`
-      el.addEventListener('transitionend', done)
-    },
-    afterDrop (el) {
-      const ball = this.dropBalls.shift()
-      if (ball) {
-        ball.show = false
-        el.style.display = 'none'
-      }
-    },
-    toggleList () {
-      if (this.listFold) {
-        if (!this.totalCount) {
-          return
+        this.$createDialog({
+          title: '支付',
+          content: `您需要支付${this.totalPrice}元`
+        }).show()
+        e.stopPropagation()
+      },
+      drop(el) {
+        for (let i = 0; i < this.balls.length; i++) {
+          const ball = this.balls[i]
+          if (!ball.show) {
+            ball.show = true
+            ball.el = el
+            this.dropBalls.push(ball)
+            return
+          }
         }
-        this.listFold = false
-        this._showShopCartList()
-      } else {
-        this.listFold = true
-        this._hideShopCartList()
+      },
+      beforeDrop(el) {
+        const ball = this.dropBalls[this.dropBalls.length - 1]
+        const rect = ball.el.getBoundingClientRect()
+        const x = rect.left - 32
+        const y = -(window.innerHeight - rect.top - 22)
+        el.style.display = ''
+        el.style.transform = el.style.webkitTransform = `translate3d(0,${y}px,0)`
+        const inner = el.getElementsByClassName(innerClsHook)[0]
+        inner.style.transform = inner.style.webkitTransform = `translate3d(${x}px,0,0)`
+      },
+      dropping(el, done) {
+        this._reflow = document.body.offsetHeight
+        el.style.transform = el.style.webkitTransform = `translate3d(0,0,0)`
+        const inner = el.getElementsByClassName(innerClsHook)[0]
+        inner.style.transform = inner.style.webkitTransform = `translate3d(0,0,0)`
+        el.addEventListener('transitionend', done)
+      },
+      afterDrop(el) {
+        const ball = this.dropBalls.shift()
+        if (ball) {
+          ball.show = false
+          el.style.display = 'none'
+        }
+      },
+      _showShopCartList() {
+        this.shopCartListComp = this.shopCartListComp || this.$createShopCartList({
+          $props: {
+            selectFoods: 'selectFoods'
+          },
+          $events: {
+            leave: () => {
+              this._hideShopCartSticky()
+            },
+            hide: () => {
+              this.listFold = true
+            },
+            add: (el) => {
+              this.shopCartStickyComp.drop(el)
+            }
+          }
+        })
+        this.shopCartListComp.show()
+      },
+      _showShopCartSticky() {
+        this.shopCartStickyComp = this.shopCartStickyComp || this.$createShopCartSticky({
+          $props: {
+            selectFoods: 'selectFoods',
+            deliveryPrice: 'deliveryPrice',
+            minPrice: 'minPrice',
+            fold: 'listFold',
+            list: this.shopCartListComp
+          }
+        })
+        this.shopCartStickyComp.show()
+      },
+      _hideShopCartList() {
+        const list = this.sticky ? this.$parent.list : this.shopCartListComp
+        list.hide && list.hide()
+      },
+      _hideShopCartSticky() {
+        this.shopCartStickyComp.hide()
       }
     },
-    _showShopCartList () {
-      this.shopCartListComp = this.shopCartListComp || this.$createShopCartList({
-        $props: {
-          selectFoods: 'selectFoods'
+    watch: {
+      fold(newVal) {
+        this.listFold = newVal
+      },
+      totalCount(count) {
+        if (!this.fold && count === 0) {
+          this._hideShopCartList()
         }
-      })
-      this.shopCartListComp.show()
+      }
     },
-    _hideShopCartList () {
-      this.shopCartListComp.hide()
+    components: {
+      Bubble
     }
-  },
-  components: {
-    Bubble
   }
-}
 </script>
 
 <style lang="stylus" scoped>
@@ -185,16 +238,13 @@ export default {
 
   .shopcart
     height: 100%
-
     .content
       display: flex
       background: $color-background
       font-size: 0
       color: $color-light-grey
-
       .content-left
         flex: 1
-
         .logo-wrapper
           display: inline-block
           vertical-align: top
@@ -207,30 +257,24 @@ export default {
           box-sizing: border-box
           border-radius: 50%
           background: $color-background
-
           .logo
             width: 100%
             height: 100%
             border-radius: 50%
             text-align: center
             background: $color-dark-grey
-
             &.highlight
               background: $color-blue
-
             .icon-shopping_cart
               line-height: 44px
               font-size: $fontsize-large-xxx
               color: $color-light-grey
-
               &.highlight
                 color: $color-white
-
           .num
             position: absolute
             top: 0
             right: 0
-
         .price
           display: inline-block
           vertical-align: top
@@ -241,35 +285,28 @@ export default {
           border-right: 1px solid rgba(255, 255, 255, 0.1)
           font-weight: 700
           font-size: $fontsize-large
-
           &.highlight
             color: $color-white
-
         .desc
           display: inline-block
           vertical-align: top
           margin: 12px 0 0 12px
           line-height: 24px
           font-size: $fontsize-small-s
-
       .content-right
         flex: 0 0 105px
         width: 105px
-
         .pay
           height: 48px
           line-height: 48px
           text-align: center
           font-weight: 700
           font-size: $fontsize-small
-
           &.not-enough
             background: $color-dark-grey
-
           &.enough
             background: $color-green
             color: $color-white
-
     .ball-container
       .ball
         position: fixed
@@ -277,7 +314,6 @@ export default {
         bottom: 22px
         z-index: 200
         transition: all 0.4s cubic-bezier(0.49, -0.29, 0.75, 0.41)
-
         .inner
           width: 16px
           height: 16px
